922c70b10141401b10b6a77090fce1ad632b86cb
Daniel Kraus <daniel.kraus@strv.com>
2018-07-09 17:38:44 +0200

    feat(_document): Add og meta tags

--- pages/_document.js
+++ pages/_document.js
@@ -3,6 +3,14 @@ import Document, { Head, Main, NextScript } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 import '../app/globalStyles'
 
+const meta = {
+  title: "STRV's Open Source Libraries",
+  description:
+    'Over the years STRV’s developers have built up quite a lot of open source libraries. This is the place where you’ll find all of them. Have fun.',
+  ogImage: 'https://strv.io/static/assets/share-banner.png',
+  ogUrl: 'https://strv.io',
+}
+
 class CustomDocument extends Document {
   static getInitialProps({ renderPage }) {
     const sheet = new ServerStyleSheet()
@@ -20,13 +28,19 @@ class CustomDocument extends Document {
       <html lang="en">
         <Head>
           <meta charSet="utf-8" />
-          <title>Open Source Libraries</title>
-          <meta name="description" content="Curated by STRV developers" />
+          <title>{meta.title}</title>
+          <meta name="description" content={meta.description} />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link rel="icon" href="/static/assets/favicon.ico" />
           <link rel="manifest" href="static/manifest.json" />
           <meta name="mobile-web-app-capable" content="yes" />
           {this.props.styleTags}
+          <meta property="og:title" content={meta.title} />
+          <meta property="og:type" content="website" />
+          <meta property="twitter:card" content="summary_large_image" />
+          <meta property="og:description" content={meta.description} />
+          <meta property="og:url" content={meta.ogUrl} />
+          <meta property="og:image" content={meta.ogImage} />
         </Head>
         <body>
           <Main />
