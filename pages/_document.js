8afb6e7f6ee851981ede729fb8b4cef98dbe1a9c
Daniel Kraus <daniel.kraus@strv.com>
2018-10-11 16:19:40 +0200

    feat: Use assets from realfavicongenerator

--- pages/_document.js
+++ pages/_document.js
@@ -24,9 +24,37 @@ export default class CustomDocument extends Document {
             rel="stylesheet"
             href="https://cdnjs.cloudflare.com/ajax/libs/10up-sanitize.css/8.0.0/sanitize.min.css"
           />
-          <link rel="icon" href="/static/assets/favicon.ico" />
-          <link rel="manifest" href="static/manifest.json" />
           <meta name="mobile-web-app-capable" content="yes" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/static/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/static/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/static/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/static/site.webmanifest" />
+          <link
+            rel="mask-icon"
+            href="/static/safari-pinned-tab.svg"
+            color="#101517"
+          />
+          <link rel="shortcut icon" href="/static/favicon.ico" />
+          <meta name="msapplication-TileColor" content="#101517" />
+          <meta
+            name="msapplication-config"
+            content="/static/browserconfig.xml"
+          />
+          <meta name="theme-color" content="#ffffff" />
           {this.props.styleTags}
         </Head>
         <body>
