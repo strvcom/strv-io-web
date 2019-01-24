315a9222bc4cebf7cbf3f4e331e3527aad0290e9
Pedro Ferreira <tonipedrof@hotmail.com>
2019-01-24 17:20:26 +0100

    Fixed footer responsiveness

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React, { Fragment, Component } from 'react'
+import React, { Fragment } from 'react'
 import PropTypes from 'prop-types'
 import { graphql } from 'gatsby'
 import queryString from 'query-string'
