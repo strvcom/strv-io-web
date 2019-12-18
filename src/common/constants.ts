1537daa537dbf1db09c5e6e147b352f09386bd7c
Fabio Moyano <fabiomoyano22@gmail.com>
2019-12-18 16:52:22 +0100

    fix: change page title

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,5 +1,5 @@
 export const SITE_METADATA = {
-  TITLE: "STRV's Open Source Libraries",
+  TITLE: "STRV's Dev Resources",
   DESCRIPTION:
     'Over the years STRV’s developers have built up quite a lot of open source libraries. This is the place where you’ll find all of them. Have fun!',
   SITE_URL: 'https://strv.io/',
