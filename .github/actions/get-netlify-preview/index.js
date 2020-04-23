f678fa241cdb07611bd3818655b7447b2d7a1633
Arnost Pleskot <arnostpleskot@gmail.com>
2020-04-23 12:55:16 +0200

    fix(ga/webpagetest): fixed get-netlify-preview action
    - Manage dapendencies out of action by adding npm install action into
      workflow (temp solution)
    - Getting all needed data from env variables
    - Waiting until deploy is really finished before ending action

--- .github/actions/get-netlify-preview/index.js
+++ .github/actions/get-netlify-preview/index.js
@@ -1,36 +1,107 @@
 const NetlifyAPI = require("netlify");
 const core = require("@actions/core");
-const github = require("@actions/github");
+const { context } = require("@actions/github");
 
-const getNeltifyPreviewURL = async () => {
+const DEPLOY_STATE = "ready";
+const DEPLOY_CONTEXT = "deploy-preview";
+const MAX_TIMEOUT = 120000;
+const DELAY = 5000;
+
+const waitUntilReady = async (deployId, client, timeout) => {
   try {
-    const client = new NetlifyAPI(core.getInput("NETLIFY_API_KEY"));
-    const siteName = core.getInput("NETLIFY_SITE_NAME");
-    const commitRef = github.context.sha;
+    // Get deploy data from client
+    const deploy = await client.getDeploy({ deploy_id: deployId });
 
-    core.debug(
-      JSON.stringify({
-        siteName,
-        commitRef,
-        context: github.context,
-      })
-    );
+    core.info("Waiting for deploy...");
+    core.debug(`Remaining timeout: ${timeout}`);
+    core.debug(`State: ${deploy.state}`);
 
-    const siteDeploys = await client.listSiteDeploys({
-      site_id: core.getInput("NETLIFY_API_ID"),
-    });
-
-    const deployId = siteDeploys.filter(
-      (deploy) =>
-        deploy.commit_ref === commitRef && deploy.context === "deploy-preview"
-    )[0].id;
-
-    core.debug(deployId);
-
-    core.setOutput("url", `https://${deployId}--${siteName}.netlify.app`);
+    // If state is not ready and action didn't timed out call this function again with delay
+    if (deploy.state !== DEPLOY_STATE && timeout > DELAY) {
+      setTimeout(
+        () => waitUntilReady(deploy.id, client, timeout - DELAY),
+        DELAY
+      );
+      // If we stop waiting and deploy is ready inform user
+    } else if (deploy.state === DEPLOY_STATE) {
+      core.info(`Deploy ${deploy.id} is ready!`);
+      // Otherwise assume that waiting timed out
+    } else {
+      core.error("Action timed out");
+    }
   } catch (err) {
-    console.error(err);
+    core.error(`Error getting deploy state: ${err.message}`);
   }
 };
 
+const getNeltifyPreviewURL = async () => {
+  // Get data from env variables
+  const apiKey = process.env.NETLIFY_API_KEY;
+  const siteName = process.env.NETLIFY_SITE_NAME;
+  const siteId = process.env.NETLIFY_API_ID;
+  // Parse timeout in case that it is not number or set fallback
+  const maxTimeout = parseInt(process.env.MAX_TIMEOUT, 10) || MAX_TIMEOUT;
+  const waitForDeploy = Boolean(process.env.WAIT_FOR_DEPLOY) || true;
+
+  core.debug(
+    JSON.stringify({
+      siteName,
+      siteId,
+      maxTimeout,
+      waitForDeploy,
+    })
+  );
+
+  // If any of mandatory env vars are not provided end action with error
+  if (!siteId || !apiKey || !siteName) {
+    core.setFailed(
+      "One of following inputs wasnt provided: NETLIFY_API_KEY, NETLIFY_SITE_NAME, NETLIFY_API_ID"
+    );
+  }
+
+  // Get commit sha from github context
+  const commitRef = context.sha;
+
+  core.debug(`Commit sha: ${commitRef}`);
+
+  // When we are out of github context end with error
+  if (!commitRef) {
+    core.setFailed("Can't get SHA of current commit.");
+  }
+
+  try {
+    // Create Netlify client
+    const client = new NetlifyAPI(apiKey);
+
+    // Get list of all deploys
+    const siteDeploys = await client.listSiteDeploys({
+      site_id: siteId,
+    });
+
+    // Find deploy that is related to current commit and is "deploy-preview"
+    const latestDeploy = siteDeploys.filter(
+      (deploy) =>
+        deploy.commit_ref === commitRef && deploy.context === DEPLOY_CONTEXT
+    )[0];
+
+    core.debug(JSON.stringify(latestDeploy));
+
+    // Compose ulr
+    const url = `https://${latestDeploy.id}--${siteName}.netlify.app`;
+
+    core.info(`Deploy URL: ${url}`);
+
+    // Set output data
+    core.setOutput("url", url);
+
+    // If deploy is not ready yet start checking deploy status
+    if (latestDeploy.state !== DEPLOY_STATE && waitForDeploy) {
+      waitUntilReady(latestDeploy.id, client, maxTimeout);
+    }
+  } catch (err) {
+    core.setFailed(err.message);
+  }
+};
+
+// Shoot
 getNeltifyPreviewURL();
