b61b7f4cf62aa0d7ea74b47c9097251c4a2e04cb
dannytce <dankijkov@gmail.com>
2020-06-10 10:07:24 +0200

    feat(lighthouse): add lighthouse assertions

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -7,5 +7,12 @@ module.exports = {
         plugins: ['lighthouse-plugin-field-performance'],
       },
     },
+    assert: {
+      // https://github.com/GoogleChrome/lighthouse-ci/blob/master/docs/configuration.md#preset
+      preset: 'lighthouse:recommended',
+      assertions: {
+        'categories:accessibility': ['error', { minScore: 1 }],
+      },
+    },
   },
 }
